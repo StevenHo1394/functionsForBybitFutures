fa5b1f6056a046888f14f7558b5683404ac29ffc
StevenHo1394 <75365123+StevenHo1394@users.noreply.github.com>
2023-03-07 17:22:49 +0800

    Add files via upload

--- bybit_futures.py
+++ bybit_futures.py
@@ -6,8 +6,6 @@ Created on Mon Mar  6 16:01:57 2023
 """
 
 import os
-import sys
-import requests
 import configparser
 from pybit import usdt_perpetual
 
@@ -17,6 +15,8 @@ PATH_PRD_KEYS = os.path.join(current_directory, "keys", "bybit_keys_real.key")
 
 BASE_URL_TEST = 'https://api-testnet.bybit.com' # testnet base url
 BASE_URL_PRD = 'https://api.bybit.com' # production base url
+
+TRADE_QTY_ROUNDOFF = 3
 
 class bybit_future:
         
@@ -43,20 +43,6 @@ class bybit_future:
     def __init__(self, testing):                
         self.set_test_env(testing) 
 
-    def get_price(self, asset):        
-        price = -1
-        response = {}
-        
-        try:
-            response = self.session_unauth.public_trading_records(symbol=asset, limit = 1)
-            
-            if ( 'ret_code' in response ) and ( response['ret_code'] == 0 ):
-                price = response['result'][0]['price']
-            
-        except Exception as e:
-            print("bybit_future.getPrice(), Exception = " + str(e))            
-        
-        return float(price)
     
     def set_leverage(self, asset, leverage):    
         result = False
@@ -72,7 +58,11 @@ class bybit_future:
         
         except Exception as e:
             print("bybit_future.setLeverage(), Exception = " + str(e))
-        
+            
+            if 'leverage not modified' or 'ErrCode: 34036' in response:
+                print("bybit_future.setLeverage(), leverage not modified!")
+                result = True
+                    
         return result, response
     
     def trade(self, asset, side, typ, price, quan):   
@@ -92,7 +82,7 @@ class bybit_future:
                 reduce_only=False,
                 close_on_trigger=False
             )
-        
+                    
             if ( response['ret_code'] == 0 ) and ( response['ext_code'] == "" ):
                 result = True
                 orderId = response['result']['order_id']
@@ -102,36 +92,72 @@ class bybit_future:
             print("bybit_future.trade(), Exception = " + str(e))
         
         return result, response, orderId
+    
+    def get_price(self, asset):
+        price = -1
+        
+        try:
+            response = self.session_unauth.public_trading_records(symbol=asset, limit = 1)
+            
+            if ( 'ret_code' in response ) and ( response['ret_code'] == 0 ):
+                price = response['result'][0]['price']
+            
+        except Exception as e:
+            print("bybit_future.get_price(), Exception = " + str(e))            
+        
+        return float(price)
+    
+    def get_trade_quantity(self, asset, collateral, leverage):        
+        price = self.get_price(asset)
+        
+        if price <= 0 :
+            return -1
+        
+        trade_quantity = -1
+        try:
+            trade_quantity = round( (float(collateral)*float(leverage)/price ), TRADE_QTY_ROUNDOFF)
+            
+        except Exception as e:
+            print("bybit_future.get_trade_quantity(), Exception = " + str(e))    
+            
+        return trade_quantity
+        
 
-    #fixme: incomplete    
+     #added the parm "order_price" in case of limit orders
     def short_token_usdt(self, symbol="BTC", leverage=5, collateral=100, order_type='Market', order_price=0): 
         
         asset = symbol + 'USDT'
+        trade_quantity = self.get_trade_quantity(asset, collateral, leverage)
         
         #set leverage
         leverage_result, leverage_response = self.set_leverage(asset, leverage)
+        print("leverage_result=" + str(leverage_result))
             
         if not leverage_result:
             return False, leverage_response, ''
                 
         #short the asset
-        return self.trade(asset, 'Short', order_type, order_price, quan) #we need order_price for limit orders               
+        return self.trade(asset, 'Sell', order_type, order_price, trade_quantity)
         
 
     #fixme: incomplete
-    def long_token_usdt(self, symbol="BTC", leverage=5, collateral=100, order_type='Market', order_price=0): 
-        return
+    # def long_token_usdt(self, symbol="BTC", leverage=5, collateral=100, order_type='Market', order_price=0): 
+        
+    #     asset = symbol + 'USDT'
+    #     trade_quantity = self.get_trade_quantity(asset, collateral, leverage)
+        
+    #     return
     
-    #fixme: incomplete
-    def close_position(self, position_id="ABC", order_type='Market', order_price=0):
-        return
+    # #fixme: incomplete
+    # def close_position(self, position_id="ABC", order_type='Market', order_price=0):
+    #     return
 
 if __name__ == '__main__':
     
     #============================== Future ======================================
     
     myBybitFut = bybit_future(testing=True) #testnet
-    #myBybitFut = bybit_future(testing=False) #production
+    #myBybitFut = bybit_future(testing=False) #real
             
     #get price
     # result = myBybitFut.get_price('BTCUSDT')
@@ -140,6 +166,17 @@ if __name__ == '__main__':
     #set leverage
     # result, response = myBybitFut.set_leverage('BTCUSDT', 5)
     # print(result)
+    
+    #get trade quantity
+    # result = myBybitFut.get_trade_quantity('BTCUSDT', 100, 5)
+    # print(result)
+    
+    #short future
+    # result, response, orderId = myBybitFut.short_token_usdt('BTC', 5, 100, order_type='Market')
+    # print(result)
+    # print(orderId)
+    
+    print('finished')
     
     
 
